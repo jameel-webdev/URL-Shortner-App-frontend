0cfa55b55b83067c13910e46e9bf1f8e40d16815
Jameel Ahmed <jameel.webdev@gmail.com>
2023-11-23 18:32:07 +0530

    Intiated URL Shortner

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div>HI WELCOME TO URL SHORTENER APP</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Url/UrlEntryPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UrlEntryPage = () => {
+  return <div>UrlEntryPage</div>;
+};
+
+export default UrlEntryPage;
